7fdf5996f2c4270624a067dacb98169fbc0983b6
PacificYZY <152719868+PacificYZY@users.noreply.github.com>
2024-03-02 18:26:34 +0800

    Update password.js

--- password.js
+++ password.js
@@ -1,7 +1,7 @@
 var div_password = '0%';
 document.documentElement.style.setProperty("--div_password_Y",div_password);
 function myFunction() {
-    if (document.getElementById("password").value == "123123") {
+    if (document.getElementById("password").value == "Pacific看上去很好吃") {
         alert("欢迎游玩😘");
         div_password = '100%';
 	    document.documentElement.style.setProperty("--div_password_Y",div_password);
